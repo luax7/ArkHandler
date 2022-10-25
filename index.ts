be570d63d38cf01553a5477b656c81ac78185727
Luan Xavier <35808867+luax7@users.noreply.github.com>
2022-10-25 17:24:46 -0300

    Delete index.ts

--- index.ts
+++
@@ -1,9 +0,0 @@
-import Command from './models/Command';
-import Feature from './models/Feature';
-import HandlerClient from './models/HandlerClient';
-import CommandHandler from './Handler'
-
-export default HandlerClient;
-export {Command, Feature};                         //Features
-export {HandlerClient,CommandHandler}              //Handlers
-
